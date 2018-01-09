d7bbd17968804f0eec21eff7e2942d32a2bdb603
Lauren Herda <lauren@laurenherda.com>
2018-01-09 18:58:01 -0500

    Ugh.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import * as _ from 'lodash'
-import * as Three from 'three'
-import * as Common from './Common'
-import FixedFOVBackdropCamera from './FixedFOVBackdropCamera'
-import AxisIndicator from './ThreeAxisIndicator'
-import ShapesList from './ShapesList'
-import PDSGrid from './PoissonDiskSamplingGrid'
+const _ = require('lodash')
+const Three = require('three')
+const Common = require('./Common')
+const FixedFOVBackdropCamera = require('./FixedFOVBackdropCamera')
+const AxisIndicator = require('./ThreeAxisIndicator')
+const ShapesList = require('./ShapesList')
+const PDSGrid = require('./PoissonDiskSamplingGrid')
 
-export default {
+module.exports = {
   template: '<div class="three-backdrop"></div>',
   props: {
     skyColor: {
