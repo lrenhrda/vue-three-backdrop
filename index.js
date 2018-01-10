ce20b118681a67d15f36e9c36b19730886d9c02e
Lauren Herda <lauren@laurenherda.com>
2018-01-09 19:09:19 -0500

    Moving things around and hopefully Babel-ifying it correctly this time.

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-import * as _ from 'lodash'
-import * as Three from 'three'
-import * as Common from './Common'
-import FixedFOVBackdropCamera from './FixedFOVBackdropCamera'
-import AxisIndicator from './ThreeAxisIndicator'
-import ShapesList from './ShapesList'
-import PDSGrid from './PoissonDiskSamplingGrid'
+'use strict';
 
-export default {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _lodash = require('lodash');
+
+var _ = _interopRequireWildcard(_lodash);
+
+var _three = require('three');
+
+var Three = _interopRequireWildcard(_three);
+
+var _Common = require('./Common');
+
+var Common = _interopRequireWildcard(_Common);
+
+var _FixedFOVBackdropCamera = require('./FixedFOVBackdropCamera');
+
+var _FixedFOVBackdropCamera2 = _interopRequireDefault(_FixedFOVBackdropCamera);
+
+var _ThreeAxisIndicator = require('./ThreeAxisIndicator');
+
+var _ThreeAxisIndicator2 = _interopRequireDefault(_ThreeAxisIndicator);
+
+var _ShapesList = require('./ShapesList');
+
+var _ShapesList2 = _interopRequireDefault(_ShapesList);
+
+var _PoissonDiskSamplingGrid = require('./PoissonDiskSamplingGrid');
+
+var _PoissonDiskSamplingGrid2 = _interopRequireDefault(_PoissonDiskSamplingGrid);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+exports.default = {
   template: '<div class="three-backdrop"></div>',
   props: {
     skyColor: {
@@ -22,8 +52,8 @@ export default {
       type: String
     },
     shapes: {
-      default: function () {
-        return ShapesList
+      default: function _default() {
+        return _ShapesList2.default;
       },
       type: Array
     },
@@ -33,196 +63,186 @@ export default {
     }
   },
   computed: {
-    renderer: function () {
+    renderer: function renderer() {
       return new Three.WebGLRenderer({
         alpha: true,
         antialias: true
-      })
+      });
     },
-    camera: function () {
-      let c = new FixedFOVBackdropCamera(
-        this.rect.width,
-        this.rect.height,
-        30,
-        0.1,
-        10000
-      )
+    camera: function camera() {
+      var c = new _FixedFOVBackdropCamera2.default(this.rect.width, this.rect.height, 30, 0.1, 10000);
 
       c.onInit = function (env) {
-        Common.centerObjectAtScroll(this)
-        this.position.z = this.getDistance()
-        this.updateProjectionMatrix()
-      }
+        Common.centerObjectAtScroll(this);
+        this.position.z = this.getDistance();
+        this.updateProjectionMatrix();
+      };
 
       c.onDraw = function (env) {
-        Common.centerObjectAtScroll(this)
-        this.updateProjectionMatrix()
-      }
+        Common.centerObjectAtScroll(this);
+        this.updateProjectionMatrix();
+      };
 
-      return c
+      return c;
     },
-    scene: function () {
-      return new Three.Scene()
+    scene: function scene() {
+      return new Three.Scene();
     },
-    axisHelper: function () {
-      return new AxisIndicator(500, 20, {
+    axisHelper: function axisHelper() {
+      return new _ThreeAxisIndicator2.default(500, 20, {
         colors: {
           origin: '#F2CE75',
           x: '#FC5A3C',
           y: '#0F9978',
           z: '#0F6899'
         }
-      })
+      });
     },
-    gridHelper: function () {
-      return new Three.GridHelper(10000, 100, 'red', 'silver')
+    gridHelper: function gridHelper() {
+      return new Three.GridHelper(10000, 100, 'red', 'silver');
     },
-    JSONLoader: function () {
-      return new Three.JSONLoader()
+    JSONLoader: function JSONLoader() {
+      return new Three.JSONLoader();
     },
-    baseMaterial: function () {
+    baseMaterial: function baseMaterial() {
       return new Three.MeshPhongMaterial({
         color: '#ffffff'
-      })
+      });
     }
   },
-  data: function () {
+  data: function data() {
     return {
       scroll: {
         top: 0,
         left: 0
       }
-    }
+    };
   },
   methods: {
-    onResize (event) {
-      this.rect = this.$el.getBoundingClientRect()
-      this.updateSize()
-      this.onDraw()
+    onResize: function onResize(event) {
+      this.rect = this.$el.getBoundingClientRect();
+      this.updateSize();
+      this.onDraw();
     },
-    onScroll (event) {
+    onScroll: function onScroll(event) {
       this.scroll = {
         top: window.scrollY,
         left: window.scrollX
-      }
-      this.onDraw()
+      };
+      this.onDraw();
     },
-    onInit (event) {
-      this.scene.traverseVisible(o => {
-        if (_.isFunction(o.onInit)) o.onInit(this)
-      })
-      this.onDraw()
+    onInit: function onInit(event) {
+      var _this = this;
+
+      this.scene.traverseVisible(function (o) {
+        if (_.isFunction(o.onInit)) o.onInit(_this);
+      });
+      this.onDraw();
     },
-    onDraw (event) {
+    onDraw: function onDraw(event) {
+      var _this2 = this;
+
       // Update simulation:
-      this.scene.traverseVisible((o) => {
-        if (_.isFunction(o.onDraw)) o.onDraw(this)
-      })
-      this.renderer.render(this.scene, this.camera)
+      this.scene.traverseVisible(function (o) {
+        if (_.isFunction(o.onDraw)) o.onDraw(_this2);
+      });
+      this.renderer.render(this.scene, this.camera);
     },
-    onFocus (event) {
-      console.log('Window has been focused.', event)
+    onFocus: function onFocus(event) {
+      console.log('Window has been focused.', event);
     },
-    getRect () {
-      return this.$el.getBoundingClientRect()
+    getRect: function getRect() {
+      return this.$el.getBoundingClientRect();
     },
-    updateSize () {
-      this.renderer.setSize(this.rect.width, this.rect.height)
-      this.camera.aspect = this.rect.width / this.rect.height
-      this.camera.updateProjectionMatrix()
-      this.onDraw()
+    updateSize: function updateSize() {
+      this.renderer.setSize(this.rect.width, this.rect.height);
+      this.camera.aspect = this.rect.width / this.rect.height;
+      this.camera.updateProjectionMatrix();
+      this.onDraw();
     },
-    loadShapes () {
-      return Promise.all(this.shapes.map(file => {
-        return new Promise((resolve, reject) => {
-          this.JSONLoader.load(file, shape => {
-            resolve(new Three.Mesh(shape, this.baseMaterial))
-          })
-        })
-      }))
+    loadShapes: function loadShapes() {
+      var _this3 = this;
+
+      return Promise.all(this.shapes.map(function (file) {
+        return new Promise(function (resolve, reject) {
+          _this3.JSONLoader.load(file, function (shape) {
+            resolve(new Three.Mesh(shape, _this3.baseMaterial));
+          });
+        });
+      }));
     },
-    generateGrid (gridShape) {
-      return new PDSGrid(gridShape, 400, {
+    generateGrid: function generateGrid(gridShape) {
+      return new _PoissonDiskSamplingGrid2.default(gridShape, 400, {
         maxDistance: 500
-      })
+      });
     }
   },
-  mounted () {
-    window.addEventListener('resize', this.onResize)
-    window.addEventListener('scroll', this.onScroll)
-    window.addEventListener('focus', this.onFocus)
+  mounted: function mounted() {
+    var _this4 = this;
 
-    this.rect = this.$el.getBoundingClientRect()
-    this.$el.appendChild(this.renderer.domElement)
-    this.updateSize()
-    this.$el.style.background = this.bgColor
-    this.scene.background = this.bgColor
+    window.addEventListener('resize', this.onResize);
+    window.addEventListener('scroll', this.onScroll);
+    window.addEventListener('focus', this.onFocus);
+
+    this.rect = this.$el.getBoundingClientRect();
+    this.$el.appendChild(this.renderer.domElement);
+    this.updateSize();
+    this.$el.style.background = this.bgColor;
+    this.scene.background = this.bgColor;
 
     // var geometry = new Three.BoxGeometry(100, 100, 100)
 
-    var lighting = new Three.HemisphereLight(
-      this.groundColor,
-      this.skyColor,
-      1
-    )
+    var lighting = new Three.HemisphereLight(this.groundColor, this.skyColor, 1);
 
-    this.camera.aspect = this.rect.width / this.rect.height
-    this.camera.updateProjectionMatrix()
+    this.camera.aspect = this.rect.width / this.rect.height;
+    this.camera.updateProjectionMatrix();
 
     // this.scene.add(this.gridHelper)
     // this.scene.add(this.axisHelper)
-    this.scene.add(lighting)
-    this.scene.add(this.camera)
+    this.scene.add(lighting);
+    this.scene.add(this.camera);
 
-    var fieldPadding = 500
-    var gridShape = new Three.Vector3(
-      Common.getDisplayWidth() + (2 * fieldPadding),
-      Common.getDocumentHeight() + (2 * fieldPadding),
-      1000
-    )
+    var fieldPadding = 500;
+    var gridShape = new Three.Vector3(Common.getDisplayWidth() + 2 * fieldPadding, Common.getDocumentHeight() + 2 * fieldPadding, 1000);
 
-    Promise.all([this.loadShapes(), this.generateGrid(gridShape)]).then(components => {
-      let meshes = components[0]
-      let grid = components[1]
-      let field = new Three.Object3D()
+    Promise.all([this.loadShapes(), this.generateGrid(gridShape)]).then(function (components) {
+      var meshes = components[0];
+      var grid = components[1];
+      var field = new Three.Object3D();
 
-      grid.forEach((point, i) => {
-        var mesh = meshes[Common.randomIntBetween(0, meshes.length - 1)].clone()
+      grid.forEach(function (point, i) {
+        var mesh = meshes[Common.randomIntBetween(0, meshes.length - 1)].clone();
 
-        var group = new Three.Group()
-        var r = Common.randomWithNegatives()
-        group.rotation.set(r * Math.PI, r * Math.PI, r * Math.PI)
-        group.position.set(point[0], point[1], point[2])
-        group.name = 'hmShape-' + i
-        mesh.scale.set(2, 2, 2)
+        var group = new Three.Group();
+        var r = Common.randomWithNegatives();
+        group.rotation.set(r * Math.PI, r * Math.PI, r * Math.PI);
+        group.position.set(point[0], point[1], point[2]);
+        group.name = 'hmShape-' + i;
+        mesh.scale.set(2, 2, 2);
 
-        if (this.rotation) {
-          let s = Common.randomWithNegatives()
+        if (_this4.rotation) {
+          var s = Common.randomWithNegatives();
           mesh.onDraw = function (env) {
-            var r = s * (window.scrollY / 1000)
-            this.rotation.set(r * Math.PI, r * Math.PI, r * Math.PI)
-          }
+            var r = s * (window.scrollY / 1000);
+            this.rotation.set(r * Math.PI, r * Math.PI, r * Math.PI);
+          };
         }
 
-        group.add(mesh)
-        field.add(group)
+        group.add(mesh);
+        field.add(group);
 
-        field.position.set(
-          -1 * fieldPadding,
-          -1 * fieldPadding,
-          0
-        )
+        field.position.set(-1 * fieldPadding, -1 * fieldPadding, 0);
 
-        this.scene.add(field)
-        this.onDraw()
-      })
-    })
+        _this4.scene.add(field);
+        _this4.onDraw();
+      });
+    });
 
-    this.onDraw()
+    this.onDraw();
   },
-  beforeDestroy () {
-    window.removeEventListener('resize', this.onResize)
-    window.removeEventListener('scroll', this.onScroll)
-    window.removeEventListener('focus', this.onFocus)
+  beforeDestroy: function beforeDestroy() {
+    window.removeEventListener('resize', this.onResize);
+    window.removeEventListener('scroll', this.onScroll);
+    window.removeEventListener('focus', this.onFocus);
   }
-}
+};
